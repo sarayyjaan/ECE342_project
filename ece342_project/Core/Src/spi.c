eed803001af3dfbf1463b0f89ea1829de80f7beb
kuangfio <kuangfionaly@gmail.com>
2023-03-23 15:42:12 -0400

    why don't we just try reading and writing it like it was lab 5 or 6 and not bother with writing to the gpio pin

--- ece342_project/Core/Src/spi.c
+++ ece342_project/Core/Src/spi.c
@@ -1,8 +1,13 @@
 #include "spi.h"
-#https://github.com/prtzl/Embedded_videos/blob/master/027_HAL-SPI/spi_examples.c
+//https://github.com/prtzl/Embedded_videos/blob/master/027_HAL-SPI/spi_examples.c
 HAL_StatusTypeDef spi_write(uint8_t regAddr, uint8_t *pData){
 	HAL_StatusTypeDef ret;
-	uint8_t send_data[2] = {regAddr, *pdata};
+	uint8_t send_data[2] = {regAddr, *pData};
 	
 	//HAL_GPIO_WritePin();
+	
+	//going to try it as if it were lab 5
+	//ret = HAL_SPI_Transmit(&hspi1, send_data, 2, 100);
+	
+	return ret;
 }
